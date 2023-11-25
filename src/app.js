7c5ebdb335f17c2b496b7632c3cbc418f960777f
SabbirSami <smd71430@gmail.com>
2023-11-25 08:25:31 +0600

    set error for unknown route

--- src/app.js
+++ src/app.js
@@ -12,6 +12,16 @@ app.get("/", (req, res) => {
     res.send("Modern Gym Running...");
 });
 
+// error handling route -------------------------------
+
+app.all("*", (req, res, next) => {
+    const error = new Error(
+        `Can't find ${req.originalUrl} route on the server`
+    );
+    error.status = 404;
+    next(error);
+});
+
 const main = async () => {
     await connectDB();
     app.listen(port, () => {
